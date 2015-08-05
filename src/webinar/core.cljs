2524e59ba2ee29d65bc656d6d03f452fba9c9542
Adrian Schaedle <adrian@schaedle.me>
2015-08-05 12:13:09 -0400

    Pull some event-munging for creating chans into functions
    These examples read the best when you have nice little functions for dom
    manipulation. This goes both ways:
    
    - DOM surgery, since this is a land without React
    - Turning DOM events into information for a channel

--- src/webinar/core.cljs
+++ src/webinar/core.cljs
@@ -3,6 +3,7 @@
   (:require [cljs.core.async :as async
              :refer [>! <! put! chan alts!]]
             [goog.events :as events]
+            [goog.dom :as dom]
             [goog.dom.classes :as classes])
   (:import [goog.events EventType]))
 
@@ -11,10 +12,9 @@
 ;; =============================================================================
 ;; Utilities
 
-(defn by-id
+(def by-id
   "Short-hand for document.getElementById(id)"
-  [id]
-  (.getElementById js/document id))
+  dom/getElement)
 
 (defn events->chan
   "Given a target DOM element and event type return a channel of
@@ -26,6 +26,11 @@
      (fn [e] (put! c e)))
    c))
 
+(defn clicks->chan
+  "Simplified events->chan function for click/touch events"
+  ([el] (clicks->chan el (chan)))
+  ([el c] (events->chan el EventType.CLICK c)))
+
 (defn mouse-loc->vec
   "Given a Google Closure normalized DOM mouse event return the
   mouse x and y position as a two element vector."
@@ -36,7 +41,7 @@
   "Given a CSS id and a message string append a child paragraph element
   with the given message string."
   [id msg]
-  (let [el (.getElementById js/document id)
+  (let [el (by-id id)
         p  (.createElement js/document "p")]
     (set! (.-innerHTML p) msg)
     (.appendChild el p)))
@@ -45,7 +50,7 @@
 ;; Example 1
 
 (defn ex1 []
-  (let [clicks (events->chan (by-id "ex1-button") EventType.CLICK)
+  (let [clicks (clicks->chan (by-id "ex1-button"))
         show!  (partial show! "ex1-messages")]
     (go
       (show! "Waiting for a click ...")
@@ -58,7 +63,7 @@
 ;; Example 2
 
 (defn ex2 []
-  (let [clicks (events->chan (by-id "ex2-button") EventType.CLICK)
+  (let [clicks (clicks->chan (by-id "ex2-button"))
         show!  (partial show! "ex2-messages")]
     (go
       (show! "Waiting for a click ...")
@@ -74,8 +79,8 @@
 ;; Example 3
 
 (defn ex3 []
-  (let [clicks-a (events->chan (by-id "ex3-button-a") EventType.CLICK)
-        clicks-b (events->chan (by-id "ex3-button-b") EventType.CLICK)
+  (let [clicks-a (clicks->chan (by-id "ex3-button-a"))
+        clicks-b (clicks->chan (by-id "ex3-button-b"))
         show!    (partial show! "ex3-messages")]
     (go
       (show! "Waiting for a click from Button A ...")
@@ -91,7 +96,7 @@
 ;; Example 4
 
 (defn ex4 []
-  (let [clicks (events->chan (by-id "ex4-button-a") EventType.CLICK)
+  (let [clicks (clicks->chan (by-id "ex4-button-a"))
         c0     (chan)
         show!  (partial show! "ex4-messages")]
     (go
@@ -108,7 +113,7 @@
 ;; Example 5
 
 (defn ex5 []
-  (let [clicks (events->chan (by-id "ex5-button") EventType.CLICK)
+  (let [clicks (clicks->chan (by-id "ex5-button"))
         c0     (chan)
         show!  (partial show! "ex5-messages")]
     (go
@@ -128,7 +133,7 @@
 
 (defn ex6 []
   (let [button (by-id "ex6-button")
-        clicks (events->chan button EventType.CLICK)
+        clicks (clicks->chan button)
         mouse  (events->chan js/window EventType.MOUSEMOVE
                  (chan 1 (map mouse-loc->vec)))
         show!  (partial show! "ex6-messages")]
@@ -152,7 +157,7 @@
 
 (defn ex7 []
   (let [button (by-id "ex7-button")
-        clicks (events->chan button EventType.CLICK)
+        clicks (clicks->chan button)
         mouse  (events->chan js/window EventType.MOUSEMOVE
                  (chan 1 (comp (map mouse-loc->vec)
                                (filter (fn [[_ y]] (zero? (mod y 5)))))))
@@ -176,7 +181,7 @@
 ;; Example 8
 
 (defn ex8 []
-  (let [clicks (events->chan (by-id "ex8-button") EventType.CLICK)
+  (let [clicks (clicks->chan (by-id "ex8-button"))
         show!  (partial show! "ex8-messages")]
     (go
       (show! "Click the button ten times!")
@@ -200,23 +205,32 @@
   [id s]
   (set! (.-innerHTML (by-id id)) s))
 
-(defn ex9 []
+(defn enable-button
+  "Removes a 'disabled' class from a button"
+  [button]
+  (classes/remove button "disabled"))
+
+(defn disable-button
+  "Sets a 'disabled' class on a button"
+  [button]
+  (classes/add button "disabled"))
+
+(defn disable-button-if
+  "Disable the button if the condition holds"
+  [condition button]
+  (if condition (disable-button button) (enable-button button)))
+
+(defn ex9 [animals]
   (let [prev-button (by-id "ex9-button-prev")
         next-button (by-id "ex9-button-next")
-        prev        (events->chan prev-button EventType.CLICK)
-        next        (events->chan next-button EventType.CLICK)
-        animals     [:aardvark :beetle :cat :dog :elk :ferret
-                     :goose :hippo :ibis :jellyfish :kangaroo]
+        prev        (clicks->chan prev-button)
+        next        (clicks->chan next-button)
         max-idx     (dec (count animals))
         set-html!   (partial set-html! "ex9-card")]
     (go
       (loop [idx 0]
-        (if (zero? idx)
-          (classes/add prev-button "disabled")
-          (classes/remove prev-button "disabled"))
-        (if (== idx max-idx)
-          (classes/add next-button "disabled")
-          (classes/remove next-button "disabled"))
+        (disable-button-if (zero? idx) prev-button)
+        (disable-button-if (== idx max-idx) next-button)
         (set-html! (nth animals idx))
         (let [[v c] (alts! [prev next])]
           (condp = c
@@ -227,7 +241,10 @@
                    (recur (inc idx))
                    (recur idx))))))))
 
-(ex9)
+
+(def animals [:aardvark :beetle :cat :dog :elk :ferret
+              :goose :hippo :ibis :jellyfish :kangaroo])
+(ex9 animals)
 
 ;; =============================================================================
 ;; Example 10
@@ -236,12 +253,8 @@
   "Given a current index and an upper bound disable
   or enable the given previous and next controls."
   [i max prev next]
-  (if (zero? i)
-    (classes/add prev "disabled")
-    (classes/remove prev "disabled"))
-  (if (== i max)
-    (classes/add next "disabled")
-    (classes/remove next "disabled")))
+  (disable-button-if (zero? i) prev)
+  (disable-button-if (== i max) next)
 
 (defn disable-buttons!
   "Given a list of buttons disable them all. The
@@ -250,7 +263,7 @@
   [[start-stop-button :as buttons]]
   (set! (.-innerHTML start-stop-button) "Done")
   (doseq [button buttons]
-    (classes/add button "disabled")))
+    (disable-button button)))
 
 (defn keys-chan
   "Return a channel of :previous and :next events
@@ -263,15 +276,15 @@
 
 (defn ex10 [animals]
   (let [start-stop-button (by-id "ex10-button-start-stop")
-        prev-button (by-id "ex10-button-prev")
-        next-button (by-id "ex10-button-next")
-        start-stop  (events->chan start-stop-button EventType.CLICK)
-        prev        (events->chan prev-button EventType.CLICK
-                      (chan 1 (map (constantly :previous))))
-        next        (events->chan next-button EventType.CLICK
-                      (chan 1 (map (constantly :next))))
-        max-idx     (dec (count animals))
-        set-html!  (partial set-html! "ex10-card")]
+        prev-button       (by-id "ex10-button-prev")
+        next-button       (by-id "ex10-button-next")
+        start-stop        (clicks->chan start-stop-button)
+        prev              (clicks->chan prev-button
+                                        (chan 1 (map (constantly :previous))))
+        next              (clicks->chan next-button
+                                        (chan 1 (map (constantly :next))))
+        max-idx           (dec (count animals))
+        set-html!         (partial set-html! "ex10-card")]
     (go
       ;; wait to start
       (<! start-stop)
@@ -298,5 +311,4 @@
                         (recur idx))
                 (recur idx)))))))))
 
-(ex10 [:aardvark :beetle :cat :dog :elk :ferret
-       :goose :hippo :ibis :jellyfish :kangaroo])
+(ex10 animals)
